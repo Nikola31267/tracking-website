62a8b46714c0b352d2251b5e158d907537627228
Nikola31267 <nikola.minchev13@gmail.com>
2025-11-25 10:52:55 +0200

    changes4

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000/api/"
-      : "https://pixeltrackapi.startgrid.xyz/api/",
+      : "https://tracking-api-lac.vercel.app/api/",
   withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": "http://localhost:3000",
